9f99792e01bc64587df829f86e98ec9aa1c83100
Mika Vainio <koti.mika.vainio@gmail.com>
2019-12-14 20:33:32 +0200

    esimerkkejä snippetteihin

--- WikiSnippets/funces.js
+++ WikiSnippets/funces.js
@@ -1,9 +1,13 @@
-// Funktion määrittely perinteinen tapa
+// Funktion määrittely perinteiseen tapaan
 function bodyMassIndex(weight, height) {
     return weight / (height * height);
 }
 
-
+// Määrittely ES6:n tapaan
+const bodyMassIndexV2 = (weight, height) => {
+    return weight / (height * height);
+}
 // Funktion käyttäminen
 let bmi = bodyMassIndex(70, 1.7);
-console.log(`bmi on ${bmi}`);
+let bmi2 = bodyMassIndexV2(77, 1.7);
+console.log(`bmi on ${bmi} ja bmi2 on ${bmi2}`);
